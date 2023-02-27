7290a189e3f8ec156c14839263bfe146534a9e57
koddan2 <97536212+koddan2@users.noreply.github.com>
2023-02-28 00:58:00 +0100

    Adds Euid

---
+++ src/Urdep.Extensions.Data/Euid.cs
@@ -0,0 +1,75 @@
+﻿using System.Globalization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Urdep.Extensions.Data;
+
+/// <summary>
+/// A struct simplifying using bytes as a somewhat human-readable identifier.
+/// </summary>
+/// <param name="Value">The string value.</param>
+[JsonConverter(typeof(EuidJsonConverter))]
+public readonly record struct Euid(string Value)
+{
+    /// <summary>
+    /// Makes an <see cref="Euid"/> from a string.
+    /// </summary>
+    /// <param name="str">The string value.</param>
+    /// <returns>The Euid value.</returns>
+    public static Euid FromString(string str) => new(str);
+
+    public static Euid FromBytes(byte[] bytes) => new(Base62.EncodingExtensions.ToBase62(bytes));
+
+    public byte[] GetByteArray() => Base62.EncodingExtensions.FromBase62(Value);
+
+    /// <summary>
+    /// Implicitly converts an <see cref="Euid"/> to a string.
+    /// </summary>
+    /// <param name="euid">The Euid value.</param>
+    public static implicit operator string(Euid euid) => euid.Value;
+
+    /// <summary>
+    /// Implicitly converts a string to an <see cref="Euid"/>.
+    /// </summary>
+    /// <param name="euid">The string value.</param>
+    public static implicit operator Euid(string str) => new(str);
+
+    /// <summary>
+    /// Implicitly converts a <see cref="System.Guid"/> to an <see cref="Euid"/>.
+    /// </summary>
+    /// <param name="g">The Guid value.</param>
+    public static implicit operator Euid(Guid g) =>
+        new(Base62.EncodingExtensions.ToBase62(g.ToByteArray()));
+
+    /// <summary>
+    /// Implicitly converts an <see cref="Euid"/> to a <see cref="System.Guid"/>.
+    /// </summary>
+    /// <param name="g">The Euid value.</param>
+    public static implicit operator Guid(Euid euid) =>
+        new(Base62.EncodingExtensions.FromBase62(euid.Value));
+
+    /// <summary>
+    /// Makes a new random <see cref="Euid"/> using <see cref="System.Guid.NewGuid"/>.
+    /// </summary>
+    /// <returns>The newly created, random, Euid.</returns>
+    public static Euid New() => Guid.NewGuid();
+
+    /// <summary>
+    /// The empty <see cref="Euid"/> value (equivalent to <see cref="System.Guid.Empty"/>).
+    /// </summary>
+    public readonly static Euid Empty = Guid.Empty;
+
+    public override string ToString() => this;
+}
+
+public class EuidJsonConverter : JsonConverter<Euid>
+{
+    public override Euid Read(
+        ref Utf8JsonReader reader,
+        Type typeToConvert,
+        JsonSerializerOptions options
+    ) => reader.GetString()!;
+
+    public override void Write(Utf8JsonWriter writer, Euid value, JsonSerializerOptions options) =>
+        writer.WriteStringValue((string)value);
+}
---
+++ src/Urdep.Extensions.Data/Test/EuidTest.cs
@@ -0,0 +1,61 @@
+﻿using NUnit.Framework;
+using System.Buffers.Text;
+using System.Text.Json;
+
+namespace Urdep.Extensions.Data.Test;
+
+public class EuidTest
+{
+    [Test]
+    public void Test1()
+    {
+        var g = new Guid("dd095a5f-dec6-434a-a0f7-a253e85fde14");
+        Euid e = g;
+        var bytes = g.ToByteArray();
+        var checkStr = Base62.EncodingExtensions.ToBase62(bytes);
+        var checkG = new Guid(Base62.EncodingExtensions.FromBase62(e.Value));
+
+        Guid g2 = e;
+        Assert.That(g, Is.EqualTo(g2));
+
+        Console.WriteLine(checkG);
+        Console.WriteLine(e);
+        Assert.That(checkStr, Is.EqualTo((string)e));
+        Assert.That(checkG, Is.EqualTo(g));
+    }
+
+    record TestData(Euid Euid);
+
+    [Test]
+    public void TestJson()
+    {
+        var g = new Guid("dd095a5f-dec6-434a-a0f7-a253e85fde14");
+        Euid e = g;
+
+        var data = new TestData(e);
+        var data2 = new TestData(e);
+        Assert.That(data, Is.EqualTo(data2));
+
+        var json = JsonSerializer.Serialize(data);
+        Assert.That(json, Is.EqualTo("{\"Euid\":\"2tvPxpmCKQi7ExfoioGDiO\"}"));
+        var back = JsonSerializer.Deserialize<TestData>(json)!;
+
+        Assert.That(back, Is.EqualTo(data));
+    }
+
+    [Test]
+    public void Test3()
+    {
+        ////var rng = new Random();
+        ////var bytes = Enumerable.Range(1, 0x1f).Select(_ => (byte)rng.Next(0xff)).ToArray();
+        ////var bytes = Guid.NewGuid().ToByteArray().Concat(Guid.NewGuid().ToByteArray()).ToArray();
+        var bytes = Guid.NewGuid().ToByteArray();
+        var e = Euid.FromBytes(bytes);
+        var b = Convert.ToBase64String(bytes);
+        var c = Convert.ToBase64String(e.GetByteArray());
+        Console.WriteLine(e);
+        Console.WriteLine(b);
+        Console.WriteLine(c);
+        Assert.That(b, Is.EqualTo(c));
+    }
+}
