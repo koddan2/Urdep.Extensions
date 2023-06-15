7c5c5d7cb57388ddfb532101053c6859157e6cc6
koddan2 <97536212+koddan2@users.noreply.github.com>
2023-06-15 23:43:12 +0200

    remove old Invoke-Build scripts

--- InvokeBuild.psm1
+++
@@ -1,7 +0,0 @@
-
-$base = (Join-Path $PSScriptRoot "Tools" "Invoke-Build")
-
-Set-Alias -Name Build-Checkpoint -Value (Join-Path $base Build-Checkpoint.ps1)
-Set-Alias -Name Build-Parallel -Value (Join-Path $base Build-Parallel.ps1)
-Set-Alias -Name Invoke-Build -Value (Join-Path $base Invoke-Build.ps1)
-Export-ModuleMember -Alias Build-Checkpoint, Build-Parallel, Invoke-Build
