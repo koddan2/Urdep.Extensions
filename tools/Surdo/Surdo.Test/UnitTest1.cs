9487f723b91350b7d04ff3ed6597f25055380768
bjorn <bjorn.roberg@gmail.com>
2025-03-06 22:19:05 +0100

    surdo init

---
+++ tools/Surdo/Surdo.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿namespace Surdo.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
